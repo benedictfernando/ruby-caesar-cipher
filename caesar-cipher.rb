faa7520a419253efc873f816e14db24ae914d1b3
benedictfernando <fernandobenedict619@gmail.com>
2022-03-20 13:12:40 +0800

    finish caesar_cipher

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,1 +1,19 @@
-puts "hello world!"
+def caesar_cipher(word, shift)
+    # convert alphabet range into an array
+    lower = ('a'..'z').to_a
+    upper = lower.map { |letter| letter.upcase }
+    letters = lower.size 
+
+    # check for each character from 'word'
+    word.chars.map do |x|
+        case true
+        when lower.include?(x) 
+            lower[lower.find_index(x) + shift - letters]
+        when upper.include?(x)
+            upper[upper.find_index(x) + shift - letters]
+        else x
+        end
+    end.join    # return output
+end
+
+puts caesar_cipher("What a string!", 5)
